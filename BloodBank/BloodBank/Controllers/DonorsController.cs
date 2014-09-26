b9b39f0cacc34c49aa94d903114068666de985c1
SeedPaths-User <lessdismissive@email.com>
2014-09-26 12:43:37 -0600

    BloodBanks

---
+++ BloodBank/BloodBank/Controllers/DonorsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPractice.Controllers
+{
+    public class DonorsController : Controller
+    {
+        //Step 1: crate a connects to our database
+        Models.BloodBanksEntities db = new Models.BlookdbanksEntities();
+
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            //list the contents of our cotact forms
+            return View(db.BloodBanks);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(db.BloodBanks.Find(id));
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(db.BloodBankss.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Models.BloodBank bloodbanks)
+        {
+             //tell the db context that the cotact form needs to be updated
+            db.Entry(bloodbanks).State = System.Data.EntityState.Modified;
+            //save changes
+            db.SaveChanges();
+
+            //kick the user back to the list
+            return RedirectToAction("Index", "Donor");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(db.Bloodbanks.Find(id));
+        }
+
+        public ActionResult DeleteConfirm(int id)
+        {
+            //get the form to delete from the database
+            Models.BloodBanks formToDelete = db.Bloodbanks.Find(id);
+
+            db.ContactForms.Remove(formToDelete);
+
+            //save changes
+            db.SaveChanges();
+
+            //kick the user back to the list
+            return RedirectToAction("Index", "Patient");
+
+
+
+        }
---
+++ BloodBank/BloodBank/Controllers/PatientsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPractice.Controllers
+{
+    public class PatientsController : Controller
+    {
+        //Step 1: crate a connects to our database
+        Models.BloodBanksEntities db = new Models.BloodBanksEntities();
+
+        //
+        // GET: /Patients/
+
+        public ActionResult Index()
+        {
+            //list the contents of our cotact forms
+            return View(db.BloodBanks);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(db.Bloodbanks.Find(id));
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(db.Bloodbanks.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Models.BloodBank bloodbanks)
+        {
+             //tell the db context that the cotact form needs to be updated
+            db.Entry(bloodbankss).State = System.Data.EntityState.Modified;
+            //save changes
+            db.SaveChanges();
+
+            //kick the user back to the list
+            return RedirectToAction("Index", "Patient");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(db.BloodBanks.Find(id));
+        }
+
+        public ActionResult DeleteConfirm(int id)
+        {
+            //get the form to delete from the database
+            Models.BloodBanks formToDelete = db.BloodBanks.Find(id);
+
+            db.Bloodbanks.Remove(formToDelete);
+
+            //save changes
+            db.SaveChanges();
+
+            //kick the user back to the list
+            return RedirectToAction("Index", "Patient");
+
+
+
+        }
